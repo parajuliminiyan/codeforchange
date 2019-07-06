2e8925d0bbfca94edd4dd625e69091dd82c3ba92
Aashan Ghimire <ashanghimire10@gmail.com>
2019-07-06 16:19:17 +0545

    Almost Completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,35 @@ $(document).ready(function(){
         $(this).addClass('active');
         $(this).css('color', '#ff0884');
     });
+
+    $(".owl-carousel").owlCarousel({
+        items : 4,
+        margin : 10,
+        loop : true,
+        rewind : true,
+        dots : true,
+        autoplay : true,
+        responsive : {
+            autoplayHoverPause : false,
+            loop : true,
+            nav : true,
+            rewind : true,
+            dots : true,
+            margin : 10,
+            0 : {
+                items : 1
+            },
+            480 : {
+                items : 2,
+            },
+            768 : {
+                items : 3,
+            },
+            1024 : {
+                items : 4
+            }
+
+        }
+    });
+
 });
